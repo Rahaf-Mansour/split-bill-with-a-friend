d7bd77cf71bb1ee19cbeed784e60e10afa4fafeb
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-12 15:33:08 +0200

    create a reusable Button component

--- src/App.jsx
+++ src/App.jsx
@@ -47,12 +47,22 @@ function FriendsList() {
 function Friend({ friend }) {
 
   return (
-    <li>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
-      {friend.balance < 0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)} $</p>}
-      {friend.balance > 0 && <p className='green'>{friend.name} owe you {Math.abs(friend.balance)} $</p>}
-      {friend.balance === 0 && <p> You and {friend.name} are even </p>}
-    </li>
+    <>
+      <li>
+        <img src={friend.image} alt={friend.name} />
+        <h3>{friend.name}</h3>
+        {friend.balance < 0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)} $</p>}
+        {friend.balance > 0 && <p className='green'>{friend.name} owe you {Math.abs(friend.balance)} $</p>}
+        {friend.balance === 0 && <p> You and {friend.name} are even </p>}
+        <Button>Select</Button>
+      </li>
+    </>
+  )
+}
+
+function Button({ children }) {
+
+  return (
+    <button className='button'>{children}</button>
   )
 }
