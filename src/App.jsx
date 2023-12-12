8f86b753372723e20c7136645de5dd9cca88074d
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-12 15:27:19 +0200

    create the Friend component and pass friend objects to it as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -39,13 +39,20 @@ function FriendsList() {
   return (
     <ul>
       {friends.map((friend) =>
-        <li>
-          <img src={friend.image}></img>
-          <h3>{friend.name}</h3>
-          {friend.balance < 0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)} $</p>}
-          {friend.balance > 0 && <p className='green'>{friend.name} owe you  {Math.abs(friend.balance)} $</p>}
-          {friend.balance === 0 && <p> You and {friend.name} are even </p>}
-        </li>)}
+        <Friend key={friend.id} friend={friend} />)}
     </ul>
+  )
+}
+
+function Friend({ friend }) {
+
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)} $</p>}
+      {friend.balance > 0 && <p className='green'>{friend.name} owe you {Math.abs(friend.balance)} $</p>}
+      {friend.balance === 0 && <p> You and {friend.name} are even </p>}
+    </li>
   )
 }
