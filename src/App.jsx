c577b49231e6e195972e83eac71c5408ca43abed
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-12-12 15:18:42 +0200

    create the FriendsList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Ali",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Maya",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Sami",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='sidebar'>
+      <FriendsList />
+    </div>
   )
 }
 
-export default App
+export default App;
+
+function FriendsList() {
+
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) =>
+        <li>
+          <img src={friend.image}></img>
+          <h3>{friend.name}</h3>
+          {friend.balance < 0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)} $</p>}
+          {friend.balance > 0 && <p className='green'>{friend.name} owe you  {Math.abs(friend.balance)} $</p>}
+          {friend.balance === 0 && <p> You and {friend.name} are even </p>}
+        </li>)}
+    </ul>
+  )
+}
